c34681eefb7a357e750c6cf9c78bfab995b9fc63
John Michael <79495944+jmbacasno@users.noreply.github.com>
2026-01-22 21:12:53 +0800

    feat(src): add filename sanitization when exporting files

--- src/exporter.py
+++ src/exporter.py
@@ -1,27 +1,30 @@
 import json
 import csv
 import time
+from dataclasses import asdict
 
 from .models import MangaList
-from .utils import manga_list_to_json_dict, manga_list_to_csv_dict
+from .utils import manga_list_custom_csv_dict, sanitize_filename
 
-#file name is current date and time 01-01-2023-01-01-01
-def import_data_to_json(manga_list: MangaList, path_folder: str):
-    filename = f"{path_folder}/{time.strftime('%Y-%m-%d-%H-%M-%S')}.json"
-    data = manga_list_to_json_dict(manga_list)
-    with open(filename, "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=4, ensure_ascii=False)
+def export_manga_list_to_json(manga_list: MangaList, path_folder: str):
+    filename = f"{path_folder}/{sanitize_filename(manga_list.title)}_{time.strftime('%Y%m%d%H%M%S')}.json"
+    data = asdict(manga_list)
+    if data:
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
 
-def import_data_to_csv(manga_list: MangaList, path_folder: str):
-    filename = f"{path_folder}/{time.strftime('%Y-%m-%d-%H-%M-%S')}.csv"
-    data = manga_list_to_csv_dict(manga_list)
-    with open(filename, mode="w", encoding="utf-8", newline="") as f:
-        writer = csv.DictWriter(
-            f,
-            fieldnames=["title", "url", "author", "genres", "cover_url", "summary", "comment"],
-            quotechar='"',
-            quoting=csv.QUOTE_ALL,
-            escapechar="\\",
-        )
-        writer.writeheader()
-        writer.writerows(data)
+def export_manga_list_to_csv(manga_list: MangaList, path_folder: str):
+    filename = f"{path_folder}/{sanitize_filename(manga_list.title)}_{time.strftime('%Y%m%d%H%M%S')}.csv"
+    data = manga_list_custom_csv_dict(manga_list)
+    if data:
+        data_fieldnames = data[0].keys()
+        with open(filename, mode="w", encoding="utf-8", newline="") as f:
+            writer = csv.DictWriter(
+                f,
+                fieldnames=data_fieldnames,
+                quotechar='"',
+                quoting=csv.QUOTE_ALL,
+                escapechar="\\",
+            )
+            writer.writeheader()
+            writer.writerows(data)
