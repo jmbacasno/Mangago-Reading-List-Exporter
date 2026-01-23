7c079f79ebb1a9276b5aafaa52312f07f7d32aae
John Michael <79495944+jmbacasno@users.noreply.github.com>
2026-01-23 16:48:15 +0800

    feat(src): improve field names for manga list custom csv dict

--- src/utils.py
+++ src/utils.py
@@ -7,14 +7,22 @@ def manga_list_custom_csv_dict(manga_list: MangaList) -> dict:
     csv_dict = []
     for manga_list_entry in manga_list.entries:
         entry = {}
-        entry["title"] = manga_list_entry.manga.title if manga_list_entry.manga else None
-        entry["url"] = manga_list_entry.manga.url if manga_list_entry.manga else None
-        entry["author"] = manga_list_entry.manga.author if manga_list_entry.manga else None
-        entry["genres"] = manga_list_entry.manga.genres if manga_list_entry.manga else None
-        entry["alternatives"] = manga_list_entry.manga.alternatives if manga_list_entry.manga else None
-        entry["cover_url"] = manga_list_entry.manga.cover_url if manga_list_entry.manga else None
-        entry["summary"] = manga_list_entry.manga.summary.replace("\n", " ") if manga_list_entry.manga else None
-        entry["comment"] = manga_list_entry.comment.replace("\n", " ") if manga_list_entry.comment else None
+        # Manga details
+        entry["manga_title"] = manga_list_entry.manga.title if manga_list_entry.manga else None
+        entry["manga_url"] = manga_list_entry.manga.url if manga_list_entry.manga else None
+        entry["manga_cover_url"] = manga_list_entry.manga.cover_url if manga_list_entry.manga else None
+        entry["manga_author"] = manga_list_entry.manga.author if manga_list_entry.manga else None
+        entry["manga_genres"] = manga_list_entry.manga.genres if manga_list_entry.manga else None
+        entry["manga_alternatives"] = manga_list_entry.manga.alternatives if manga_list_entry.manga else None
+        entry["manga_summary"] = manga_list_entry.manga.summary.replace("\n", " ") if manga_list_entry.manga else None
+        entry["manga_status"] = manga_list_entry.manga.status if manga_list_entry.manga else None
+        entry["manga_released_year"] = manga_list_entry.manga.released_year if manga_list_entry.manga else None
+        entry["manga_rating"] = manga_list_entry.manga.rating if manga_list_entry.manga else None
+        entry["manga_votes"] = manga_list_entry.manga.votes if manga_list_entry.manga else None
+        # Entry details
+        entry["entry_comment"] = manga_list_entry.comment.replace("\n", " ") if manga_list_entry.comment else None
+        entry["entry_add_date"] = manga_list_entry.add_date
+        
         csv_dict.append(entry)
     
     return csv_dict
